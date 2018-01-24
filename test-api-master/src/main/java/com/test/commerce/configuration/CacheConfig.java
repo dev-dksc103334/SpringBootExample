48dfebd5c8634493fe50a709e4833717580fe691
Baranidharan Ramachandran <dev-dksc103334@dcsg.com>
2018-01-24 12:34:57 -0500

    Sample

---
+++ test-api-master/src/main/java/com/test/commerce/configuration/CacheConfig.java
@@ -0,0 +1,73 @@
+package com.test.commerce.configuration;
+
+import java.lang.reflect.Method;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+@EnableCaching
+@PropertySource("application.properties")
+public class CacheConfig extends CachingConfigurerSupport {
+	
+	@Value("${spring.redis.host}")
+    private String redisHostName;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+    
+  @Bean
+  public JedisConnectionFactory redisConnectionFactory() {
+    JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+    redisConnectionFactory.setHostName(redisHostName);
+    redisConnectionFactory.setPort(redisPort);
+    return redisConnectionFactory;
+  }
+
+  @Bean
+  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+    redisTemplate.setConnectionFactory(cf);
+    return redisTemplate;
+  }
+  @Bean
+  public StringRedisTemplate redisTemplate() {
+      StringRedisTemplate redisTemplate = new StringRedisTemplate(redisConnectionFactory());
+      return redisTemplate;
+  }
+  @Bean
+  public CacheManager cacheManager(RedisTemplate redisTemplate) {
+    RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+    cacheManager.setDefaultExpiration(300);
+    cacheManager.setUsePrefix(true);
+    return cacheManager;
+  }
+
+  @Bean
+  public KeyGenerator customKeyGenerator() {
+    return new KeyGenerator() {
+      @Override
+      public Object generate(Object o, Method method, Object... objects) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(o.getClass().getName());
+        sb.append(method.getName());
+        for (Object obj : objects) {
+          sb.append(obj.toString());
+        }
+        return sb.toString();
+      }
+    };
+  }
+
+}
---
+++ test-api-master/src/main/java/com/test/commerce/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.test.commerce.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/v4/store/{storeid}")
+@ComponentScan(basePackages="com.dsg.commerce.configuration")
+public class TestController{
+	 	@RequestMapping("/")
+	    @ResponseBody
+	    @Cacheable(value="home")
+	    public String home() {
+	        return "Hello World!";
+	    }
+	 	
+	 	@RequestMapping(value = "/calculate", method = RequestMethod.GET)
+	 	@ResponseBody
+	 	@Cacheable("calculateResult")
+	    public String calculateResult() {
+	 	    return "result";
+	 	}
+	 	
+	 	public static void main(String[] args) {
+	        SpringApplication.run(TestController.class, args);
+	    }
+
+}
